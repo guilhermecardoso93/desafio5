c55dcabc204bbb49bef3e2b0bd871a397274d376
Guilherme Cardoso <guilhermecardosowork@gmail.com>
2022-02-07 22:06:52 -0300

    style finished and API  GitHub

---
+++ script.js
@@ -0,0 +1,19 @@
+function githbInfos() {
+    const url = `https://api.github.com/users/guilhermecardoso93`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            twitter.textContent = data.twitter_username,
+                projetos.textContent = data.public_repos + " repositórios",
+                userName.textContent = data.name,
+                avatarUrl.src = data.avatar_url,
+                login.textContent = data.login,
+                followers.textContent = data.followers + " seguidores",
+                following.textContent = data.following + " seguindo",
+                city.textContent = data.location
+            loginGit.href = data.html_url
+        })
+}
+
+githbInfos()
